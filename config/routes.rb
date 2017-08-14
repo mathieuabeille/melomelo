8f7686c22845df2844d1101f3976ccfff454c5cd
mathieu abeille <mathieu.abeille@gmail.com>
2017-08-14 17:42:51 +0200

    route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,24 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :clothes, only: [:index, :show,:new, :create, :edit, :update, :destroy] do
+    resources :baskets, only: [:create, :edit, :update, :destroy]
+  end
+  resources :baskets, only: :index
 end
+
+
+
+# Rails.application.routes.draw do
+#   devise_for :users,
+#     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+#   resources :stages, only: [:index, :show,:new, :create, :edit, :update, :destroy] do
+#     resources :bookings, only: [ :create ]
+#   end
+
+#   resources :bookings, only: :index
+
+#   root to: 'pages#home'
+#   get '/me/stage', to: 'stages#mystage'
+# end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814140646) do
+ActiveRecord::Schema.define(version: 20170814150550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20170814140646) do
     t.string "color"
     t.float  "price",                    null: false
     t.string "tags",        default: [],              array: true
+    t.string "brand",                    null: false
+    t.string "material"
   end
 
   create_table "users", force: :cascade do |t|
