27d59e10d4bec267e7335120fb69a569a7e2d4e4
mouterde adeline <adeline.mouterde@free.fr>
2017-08-15 11:12:05 +0200

    view index V1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,71 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Cloth.destroy_all
+
+clothes_attributes = [
+  {
+    name: "Embroid T-Shirt",
+    description: "embroid",
+    size: "M",
+    price: 20,
+    color: "white",
+    brand: "ZARA",
+    material: "cotton",
+  },
+
+  {
+    name: "Embroid T-Shirt",
+    description: "rose",
+    size: "M",
+    price: 15,
+    color: "Beige",
+    brand: "H&M",
+    material: "cotton",
+  },
+
+  {
+    name: "T-Shirt",
+    description: "embroid",
+    size: "M",
+    price: 30,
+    color: "Beige",
+    brand: "ASOS",
+    material: "polyester",
+  },
+
+  {
+    name: "T-Shirt",
+    description: "embroid",
+    size: "M",
+    price:40,
+    color: "Beige",
+    brand: "AMAZON",
+    material: "cotton",
+  },
+
+  {
+    name: "T-Shirt",
+    description: "embroid",
+    size: "M",
+    price:20,
+    color: "white",
+    brand: "AMAZON",
+    material: "polyester",
+
+  },
+
+  {
+    name: "T-Shirt",
+    description: "embroid",
+    size: "M",
+    price:40,
+    color: "black",
+    brand: "ASOS",
+    material: "cotton",
+
+  }
+]
+
+puts "Creating Clothes"
+Cloth.create!(clothes_attributes)
+
+
+
