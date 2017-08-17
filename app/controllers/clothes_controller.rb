4e2b279fc8dd10328032a5c9afa0974a1c882160
mouterde adeline <adeline.mouterde@free.fr>
2017-08-17 17:36:48 +0200

    view and bit of filter

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -2,6 +2,8 @@ class ClothesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :destroy]
 
   def index
+    #@clothes = ProductFilter.new(params).filter
     @clothes = Cloth.all
+
   end
 end
