95d691d1fafb20dcba64eea9e00c62366a55489d
mouterde adeline <adeline.mouterde@free.fr>
2017-08-18 12:21:25 +0200

    filtres debut

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -2,7 +2,7 @@ class ClothesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :destroy]
 
   def index
-    #@clothes = ProductFilter.new(params).filter
+    #@clothes = ClothesFilter.new(params).filter
     @clothes = Cloth.all
 
   end
--- app/services/clothes_filter.rb
+++ app/services/clothes_filter.rb
@@ -1,7 +1,4 @@
 class ClothesFilter
-  SIZES = {
-    small:
-  }
 
   def initialize(params)
     @params = params
@@ -9,19 +6,15 @@ class ClothesFilter
   end
 
   def filter(params)
-    filter_by_size
-    # filter_by_category
-    # filter_by_color
-    # filter_by_price
+    filter_by_price
+    filter_by_gender
+    filter_by_color
+    filter_by_ascending
 
     @clothes
   end
 
-  def filter_by_size
-    if params[:size].present?
-      # @clothes = @clothes.where("sizes IN ?", []) if params[:price] == "low"
-    end
-  end
+
 
   def filter_by_price
     if params[:price].present?
@@ -29,6 +22,32 @@ class ClothesFilter
       @clothes = @clothes.where("price >= ? and price < ?", 50, 150) if params[:price] == "middle"
       @clothes = @clothes.where("price >= ?", 150) if params[:price] == "high"
     end
+  end
+
+  def filter_by_gender
+    if params[:gender].present?
+      @clothes = @clothes.where("gender ?", male) if params[:price] == "male"
+      @clothes = @clothes.where("gender?", female) if params[:price] == "female"
+    end
+  end
+
+  def filter_by_color
+    if params[:color].present?
+      @clothes = @clothes.where("color ?", black) if params[:price] == "black"
+      @clothes = @clothes.where("color?", blue) if params[:price] == "blue"
+      @clothes = @clothes.where("color?", orange) if params[:price] == "orange"
+      @clothes = @clothes.where("color?", purple) if params[:price] == "purple"
+      @clothes = @clothes.where("color?", red) if params[:price] == "red"
+      @clothes = @clothes.where("color?", yellow) if params[:price] == "yellow"
+      @clothes = @clothes.where("color?", white) if params[:price] == "white"
+    end
+  end
+
+  def filter_by_ascending
+    if params[:price_asc_desc].present?
+      @clothes = @clothes.order(price: :asc) if params[:price] == "ascending"
+    else
+      @clothes = @clothes.order(price: :desc) if params[:price] == "descending"
   end
 
 
