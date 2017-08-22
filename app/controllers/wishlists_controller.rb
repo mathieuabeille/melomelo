b1bcce2de225ad10c5ab6abb68f3a2186794325f
mathieu abeille <mathieu.abeille@gmail.com>
2017-08-22 15:46:28 +0200

    v1

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,2 +1,14 @@
 class WishlistsController < ApplicationController
+  skip_before_action :authenticate_user, only [:index, :edit, ]
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
 end
