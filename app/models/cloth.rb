995c01ab7757448bd9e671c534c5eccea340db42
mathieu abeille <mathieu.abeille@gmail.com>
2017-08-22 18:10:08 +0200

    ve

--- app/models/cloth.rb
+++ app/models/cloth.rb
@@ -1,3 +1,22 @@
 class Cloth < ApplicationRecord
   self.table_name = 'clothes'
+
+
+  # after_create :match_check
+
+
+  # private
+
+  # def match_check
+  #   if self.match?()
+  #     return #action d'envoyer un email.
+  #   else
+  #     return pu
+  #   end
+  end
 end
+
+
+# #pour color
+
+# Wish each do
