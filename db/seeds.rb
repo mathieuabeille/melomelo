01b63903b58aa781043d31f503a82cb83ce57c7a
mouterde adeline <adeline.mouterde@free.fr>
2017-08-23 15:51:02 +0200

    form pour mathieu

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,39 +9,10 @@ user = User.create!(
   last_name: "Doe",
 )
 
-wishes_attributes = [
-  {
-    name: "T-Shirt",
-    price: "20",
-    color:"blue",
-    gender: "man",
-    material: "Coton",
-    user: user
-  },
-
-  {
-    price: "60",
-    color:"black",
-    gender: "women",
-    name: "Dress",
-    material: "linen",
-    user: user
-  },
-
-
-  {
-    price: "100",
-    color:"blue",
-    gender: "women",
-    name: "jeans",
-    description: "embroid",
-    user: user
-  },
-]
 
 
 puts "Everything done"
-Wish.create!(wishes_attributes)
+
 
 
 
